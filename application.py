11c82a8938fce7641b41768634670759c8e61103
Saini19 <123344382+ASaini09@users.noreply.github.com>
2023-02-09 17:46:37 -0700

    Final Product
    I had some errors with pushing to GitHub. This is the final submission. The code now functions as intended. After completing task 3, the user could register, log in, and search for any book through its ISBN, Title, or Author name. After the user searches, the home page returns a list of all relevant books. The user can then click on any ISBN code and go to that corresponding books "book" page. Here, all details of the book are listed. Furthermore, the user can view any reviews that any users have left for the book on the website. The user can also leave a review themselves, provided they input their username.

--- application.py
+++ application.py
@@ -104,7 +104,7 @@ def logout():
 
 	return render_template('logout.html')
 
-@app.route("/book/<string:isbn>")
+@app.route("/book/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
 
 	cursor = connection.cursor()
@@ -114,10 +114,17 @@ def book(isbn):
 	cursor.execute(query)
 	book = cursor.fetchone()
 
-	# displaying all reviews for the book
-	review_input = f"SELECT review FROM reviews WHERE isbn='{isbn}'"
-	cursor.execute(review_input)
+	# displaying the selected books reviews
+	query = f"SELECT * FROM reviews WHERE isbn='{isbn}'"
+	cursor.execute(query)
 	reviews = cursor.fetchall()
+
+	if request.method == "POST":
+		user = request.form.get("user")
+		review = request.form.get("review")
+		query = f"INSERT INTO reviews (isbn, review, username) VALUES ('{isbn}', '{review}', '{user}')"
+		cursor.execute(query)
+		connection.commit()
 
 	return render_template("book.html", book=book, reviews=reviews)
 
